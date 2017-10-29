c5a0ae8e627b9e001b899506d2a10be7913164c6
yarivdo <yarivdo@gmail.com>
2017-10-29 14:02:27 +1300

    Worling framework, before logic. all buttons worlk

--- myJS.js
+++ myJS.js
@@ -1,5 +1,30 @@
+// Global variables
+var myCells;
+
+
 $(document).ready(startHere);
 
 function startHere() {
+    // Restart the array
 
+    myCells = {
+    "11" : "*",
+    "12" : "*",
+    "13" : "*",
+    "21" : "*",
+    "22" : "*",
+    "23" : "*",
+    "31" : "*",
+    "32" : "*",
+    "33" : "*"
+    };
+
+
+    $('.cells').click(cellClicked);
+
+}
+
+function cellClicked() {
+    var whichCell = $(this).attr('data-button');
+    console.log(whichCell);
 }
